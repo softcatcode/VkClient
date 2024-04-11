892864aab07456b7848545d1d3fe375d3dfba755
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-04-11 13:42:50 +0300

    token is fetched

--- app/src/main/java/com/softcatcode/vkclient/presentation/viewModels/AuthViewModel.kt
+++ app/src/main/java/com/softcatcode/vkclient/presentation/viewModels/AuthViewModel.kt
@@ -1,19 +1,28 @@
 package com.softcatcode.vkclient.presentation.viewModels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.softcatcode.vkclient.domain.entities.states.AuthState
 import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKPreferencesKeyValueStorage
+import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.api.sdk.auth.VKAuthenticationResult
 
-class AuthViewModel: ViewModel() {
+class AuthViewModel(
+    private val application: Application
+): AndroidViewModel(application) {
 
     private val _state = MutableLiveData<AuthState>()
     val state: LiveData<AuthState> = _state
 
     init {
-        _state.value = if (VK.isLoggedIn()) AuthState.Authorized else AuthState.NotAuthorized
+        val storage = VKPreferencesKeyValueStorage(application)
+        val token = VKAccessToken.restore(storage)
+        val loggedIn = token != null && token.isValid
+        _state.value = if (loggedIn) AuthState.Authorized else AuthState.NotAuthorized
     }
 
     fun performAuthResult(result: VKAuthenticationResult) {
