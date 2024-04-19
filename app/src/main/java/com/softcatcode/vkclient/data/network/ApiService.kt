a433a2d700719d15768592cee32784d1d2e27fc3
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-04-19 17:31:00 +0300

    Like set/reset api was added.

--- app/src/main/java/com/softcatcode/vkclient/data/network/ApiService.kt
+++ app/src/main/java/com/softcatcode/vkclient/data/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.softcatcode.vkclient.data.network
 
+import com.softcatcode.vkclient.data.model.LikeCountResponseDto
 import com.softcatcode.vkclient.data.model.NewsFeedResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,6 +10,20 @@ interface ApiService {
     suspend fun getRecommendations(
         @Query("access_token") token: String
     ): NewsFeedResponseDto
+
+    @GET("likes.add?v=$VERSION&type=post")
+    suspend fun like(
+        @Query("access_token") token: String,
+        @Query("owner_id") ownerId: Long,
+        @Query("post_id") postId: Long
+    ): LikeCountResponseDto
+
+    @GET("likes.delete?v=$VERSION&type=post")
+    suspend fun dislike(
+        @Query("access_token") token: String,
+        @Query("owner_id") ownerId: Long,
+        @Query("post_id") postId: Long
+    ): LikeCountResponseDto
 
     companion object {
         private const val VERSION = "5.199"
