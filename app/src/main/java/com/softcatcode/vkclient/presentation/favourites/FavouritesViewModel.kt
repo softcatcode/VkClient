733b438eea510e3314f827e76c7ef99dca252ef7
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-04 20:27:54 +0300

    FavouritesViewModel was added.

--- app/src/main/java/com/softcatcode/vkclient/presentation/favourites/FavouritesViewModel.kt
+++ app/src/main/java/com/softcatcode/vkclient/presentation/favourites/FavouritesViewModel.kt
@@ -1,45 +1,26 @@
 package com.softcatcode.vkclient.presentation.favourites
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.softcatcode.vkclient.domain.useCase.ChangeLikeStatusUseCase
 import com.softcatcode.vkclient.domain.useCase.GetFavouritesUseCase
+import com.softcatcode.vkclient.domain.useCase.GetRecommendationsUseCase
+import com.softcatcode.vkclient.domain.useCase.IgnorePostUseCase
 import com.softcatcode.vkclient.domain.useCase.LoadNextFavouritesUseCase
-import com.softcatcode.vkclient.presentation.extensions.mergeWith
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.launch
+import com.softcatcode.vkclient.domain.useCase.LoadNextPostsUseCase
+import com.softcatcode.vkclient.domain.useCase.RemoveFromFavouritesUseCase
+import com.softcatcode.vkclient.presentation.home.news.NewsViewModel
 import javax.inject.Inject
 
 class FavouritesViewModel @Inject constructor(
     private val loadNextFavouritesUseCase: LoadNextFavouritesUseCase,
-    getFavouritesUseCase: GetFavouritesUseCase,
-): ViewModel() {
+    private val getFavouritesUseCase: GetFavouritesUseCase,
+    private val removeFromFavouritesUseCase: RemoveFromFavouritesUseCase,
+    getRecommendationsUseCase: GetRecommendationsUseCase,
+    loadNextPostsUseCase: LoadNextPostsUseCase,
+    ignorePostUseCase: IgnorePostUseCase,
+    changeLikeStatusUseCase: ChangeLikeStatusUseCase
+): NewsViewModel(getRecommendationsUseCase, loadNextPostsUseCase, ignorePostUseCase, changeLikeStatusUseCase) {
 
-    private val favouritesFlow = getFavouritesUseCase()
-    private val updatedStateFlow = MutableSharedFlow<FavouritesScreenState>()
-
-    val state = favouritesFlow
-        .filter { it.isNotEmpty() }
-        .map { FavouritesScreenState.Content(posts = it) as FavouritesScreenState }
-        .onStart { emit(FavouritesScreenState.Loading) }
-        .mergeWith(updatedStateFlow)
-
-    init {
-        loadFavourites()
-    }
-
-    private fun loadFavourites() {
-        viewModelScope.launch(Dispatchers.IO) {
-            updatedStateFlow.emit(
-                FavouritesScreenState.Content(
-                    posts = favouritesFlow.value,
-                    nextLoading = true
-                )
-            )
-            loadNextFavouritesUseCase()
-        }
-    }
+    override fun invokeGetPostsUseCase() = getFavouritesUseCase()
+    override suspend fun invokeLoadNextUseCase() = loadNextFavouritesUseCase()
+    override suspend fun invokeRemovePostUseCase(id: Long) = removeFromFavouritesUseCase()
 }
